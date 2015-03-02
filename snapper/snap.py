bd91e3089ec497384e0c4cf3798a688fa21fd7ce
Grahame Bowland <grahame@angrygoats.net>
2015-03-03 01:08:29 +0800

    temp logging

--- snapper/snap.py
+++ snapper/snap.py
@@ -1,5 +1,8 @@
 import datetime
+import glob
 import time
+import csv
+import sys
 import os
 from subprocess import call
 from astral import Astral
@@ -10,12 +13,38 @@ def safe_mkdir(d):
     except OSError:
         pass
 
+class ThermalLogs(object):
+    def __init__(self, directory):
+        def outf(f):
+            return os.path.join(directory, f.split('/')[-2] + '.csv')
+        self.probe_files = dict((outf(t), t) for t in glob.glob('/sys/bus/w1/devices/28-*/w1_slave'))
+
+    def read_dev(self, dev):
+        with open(dev) as fd:
+            lines = [t.strip() for t in fd.readlines()]
+            if len(lines) == 2 and lines[0].endswith(' YES'):
+                try:
+                    return float(lines[1].split('=')[-1]) / 1000
+                except ValueError:
+                    return None
+            return None
+
+    def update(self):
+        vals = []
+        for fname, dev in sorted(self.probe_files.items()):
+            now, temp = time.time(), self.read_dev(dev)
+            vals.append(temp)
+            with open(fname, 'a') as fd:
+                w = csv.writer(fd)
+                w.writerow([now, temp])
+        return vals
 
 class Timelapse(object):
     def __init__(self, directory, city_data, interval):
         self.tz = city_data.tz
         self.date = datetime.datetime.now(tz=self.tz)
         self.sun = city_data.sun(self.date, local=True)
+        self.thermal = ThermalLogs(directory)
         self.directory = os.path.join(
                 directory,
                 self.date.strftime("%Y-%m-%d"))
@@ -38,9 +67,19 @@ class Timelapse(object):
         code = call(args)
         return code, time.time() - now
 
+    def timed_thermal(self):
+        now = time.time()
+        vals = self.thermal.update()
+        return vals, time.time() - now
+
+    def write_flush(self, s):
+        sys.stdout.write(s)
+        sys.stdout.flush()
+
     def acquire(self):
         while True:
             now = datetime.datetime.now(tz=self.tz)
+            self.write_flush("acquire: ")
             if not self.time_today(now):
                 return
             timestamp = time.mktime(now.timetuple())
@@ -51,11 +90,13 @@ class Timelapse(object):
             mode = self.getmode(now)
             if mode == 'night':
                 args += ['--shutter', '200000', '-awb', 'off', '-awbg', '1,1']
-            rv, elapsed = self.timed_call(args)
-            print '%s (mode %s, code %d, elapsed %.1fs)' % (outf, mode, rv, elapsed)
-            sleep_time = self.interval - elapsed
+            rv, shoot_elapsed = self.timed_call(args)
+            vals, thermal_elapsed = self.timed_thermal()
+            sleep_time = self.interval - shoot_elapsed - thermal_elapsed
+            self.write_flush('%s (%s, %d, s=%.1fs, t=%.1fs, %s). wait %.1fs\n' % (
+                os.path.basename(outf), mode, rv, shoot_elapsed, thermal_elapsed, vals, sleep_time))
             if sleep_time > 0:
-                time.sleep(10)
+                time.sleep(sleep_time)
 
 
 def go(city, directory, interval):
