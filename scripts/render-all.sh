c94ac827b00d941d0a73fe9536094cef54d03f17
Grahame Bowland <grahame@angrygoats.net>
2015-04-02 20:37:50 +0800

    daily rendering tempfile

--- scripts/render-all.sh
+++ scripts/render-all.sh
@@ -15,7 +15,11 @@ dirs() {
 for i in $( dirs ); do
     b="`basename $i`"
     movie=$base"daily/$b".mp4
+    tmpmovie="$movie".tmp
     if [ ! -f "$movie" ]; then
-        ./render.sh "$i" 24 "$movie"
+        echo "rendering: $i"
+        ./render.sh "$i" 24 "$tmpmovie" && mv "$tmpmovie" "$movie"
+    else
+        echo "already done: $i"
     fi
 done
