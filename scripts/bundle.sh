4580127f734ca073b70e3c4f355ac23fe6b6fe73
Grahame Bowland <grahame@angrygoats.net>
2015-04-21 22:28:56 +0800

    bundle max 10 files at a time

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,6 +2,6 @@
 
 cd ~/snaps/
 rmdir ????-??-??/ >/dev/null 2>&1
-bundle=$(find ????-??-?? -type f -name "*.jpg")
+bundle=$(find ????-??-?? -type f -name "*.jpg" | head)
 out=bundles/"$$.$RANDOM.tar"
 tar cf "$out".tmp $bundle && mv "$out".tmp "$out" && rm $bundle
