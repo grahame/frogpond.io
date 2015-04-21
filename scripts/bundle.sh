ee43293c942625b275b33759950205af86b15927
Grahame Bowland <grahame@angrygoats.net>
2015-04-21 22:42:18 +0800

    increase bundle size

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,6 +2,6 @@
 
 cd ~/snaps/
 rmdir ????-??-??/ >/dev/null 2>&1
-bundle=$(find ????-??-?? -type f -name "*.jpg" | head)
+bundle=$(find ????-??-?? -type f -name "*.jpg" | head -100)
 out=bundles/"$$.$RANDOM.tar"
 tar cf "$out".tmp $bundle && mv "$out".tmp "$out" && rm $bundle
