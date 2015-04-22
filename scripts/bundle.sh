ca34bb0a5f247eba91eca71e9e12a257a272f28e
Grahame Bowland <grahame@angrygoats.net>
2015-04-23 00:16:11 +0800

    update bundle

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,6 +2,5 @@
 
 cd ~/snaps/
 rmdir ????-??-??/ >/dev/null 2>&1
-bundle=$(find ????-??-?? -type f -name "*.jpg" | head -100)
-out=bundles/"$$.$RANDOM.tar"
-tar cf "$out".tmp $bundle && mv "$out".tmp "$out" && rm $bundle
+bundle=$(find ????-??-?? -type f -name "*.jpg" | head -200)
+tar cf - $bundle && rm $bundle
