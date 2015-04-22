9a6c30f4205ad6030fecbb32ccae7e94754107d6
Grahame Bowland <grahame@angrygoats.net>
2015-04-23 00:16:39 +0800

    pull tar

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,6 +2,9 @@
 
 base="/Volumes/Frogpond/snaps"
 host="frogpond2.local"
+spleep=10
+
+set -e
 
 while true; do
     if [ ! -d "$base" ]; then
@@ -9,17 +12,8 @@ while true; do
         sleep 60
         continue
     fi
-    echo "bundling photos on pi" &&
-    ssh pi@"$host" "~/code/frogpond.io/scripts/bundle.sh" &&
-    echo "grabbing bundles" &&
-    rsync -av --progress pi@"$host":snaps/bundles/ "$base"/bundles/ &&
-    echo "extract" &&
-    (cd "$base" &&
-     for i in ./bundles/*.tar; do
-         tar xvf "$i" && rm "$i"
-     done) &&
-    echo "cleanup" &&
-    ssh pi@frogpond2.local "rm ~/snaps/bundles/*.tar"
-    echo "sleep"
-    sleep 10
+    echo "grabbing a bundle of photos from pi" &&
+    (ssh pi@"$host" "~/code/frogpond.io/scripts/bundle.sh" | (cd "$base" && tar xvf -) )
+    echo "sleep $spleep" &&
+    sleep $spleep
 done
