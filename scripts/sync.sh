15b0ba6db7353d042cb50e943376077fea90ecbf
Grahame Bowland <grahame@angrygoats.net>
2015-04-27 17:02:20 +0800

    pull from multiple output dirs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,14 +6,20 @@ spleep=10
 
 set -e
 
+grab() {
+    echo "grabbing a bundle of photos from pi: $1" &&
+    (ssh pi@"$host" "~/code/frogpond.io/scripts/bundle.sh" "$1" | (cd "$base" && tar xvf -) )
+}
+
 while true; do
     if [ ! -d "$base" ]; then
         echo "frogpond not mounted, waiting..."
         sleep 60
         continue
     fi
-    echo "grabbing a bundle of photos from pi" &&
-    (ssh pi@"$host" "~/code/frogpond.io/scripts/bundle.sh" | (cd "$base" && tar xvf -) )
+    grab "/home/pi/snaps/"
+    grab "/usb/snaps/"
+    rsync -av --progress pi@"$host":"~/snaps/*.csv" "$base"/
     echo "sleep $spleep" &&
     sleep $spleep
 done
